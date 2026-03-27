0ba3da02378cbfd31f5a3f7b4521f4a5517f5abb
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-27 22:57:19 +0530

    Working on answer Api

--- src/main/java/com/work/SkillClash/controller/MainOperationsController.java
+++ src/main/java/com/work/SkillClash/controller/MainOperationsController.java
@@ -5,9 +5,7 @@ import com.work.SkillClash.service.MainOperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,10 @@ public class MainOperationsController {
         return new ResponseEntity<>(listQue,HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<String> sendAns(@RequestBody String qId,String ans){
+
+        return new ResponseEntity("Correct",HttpStatus.OK);
+    }
 
 }
