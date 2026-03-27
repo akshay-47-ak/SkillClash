2b108103b9088eb53a364f954f81e52707fdecfd
akshay-47-ak <akshayathavale1525@gmail.com>
2026-03-27 21:40:12 +0530

    Created the New Project SkillClash

---
+++ src/main/java/com/work/SkillClash/SkillClashApplication.java
@@ -0,0 +1,13 @@
+package com.work.SkillClash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillClashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillClashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/SkillClash/SkillClashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.SkillClash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillClashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
