c323e40c2f069879fa3baceb534cecaa50676bef
akshay-47-ak <akshayathavale1525@gmail.com>
2026-04-07 20:33:44 +0530

    Calculated Score API

--- src/main/java/com/work/SkillClash/controller/AnswerAndScoreController.java
+++ src/main/java/com/work/SkillClash/controller/AnswerAndScoreController.java
@@ -5,6 +5,7 @@ import com.work.SkillClash.service.AnswerAndScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class AnswerAndScoreController {
     @Autowired
     AnswerAndScoreService answerAndScoreService;
 
+    @PostMapping("/sendAns")
     public ResponseEntity<String> sendAnswer(
             @RequestBody List<AnswerResponse> listAns
      ){
@@ -27,10 +29,10 @@ public class AnswerAndScoreController {
            ansMap.put(ar.getQId(),ar.getAns());
         }
 
-
         String score = answerAndScoreService.getCalculateScore(ansMap);
 
-        return new ResponseEntity<>("Ans", HttpStatus.OK);
+        String calculatedScore = "Your Score is "+score;
+        return new ResponseEntity<>(calculatedScore, HttpStatus.OK);
     }
 
 
--- src/main/java/com/work/SkillClash/service/AnswerAndScoreServiceImpl.java
+++ src/main/java/com/work/SkillClash/service/AnswerAndScoreServiceImpl.java
@@ -1,23 +1,31 @@
 package com.work.SkillClash.service;
 
 
+import com.work.SkillClash.dto.AnswerResponse;
+import com.work.SkillClash.model.Question;
+import com.work.SkillClash.repository.QuestionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class AnswerAndScoreServiceImpl implements AnswerAndScoreService{
 
     @Autowired
-
+    QuestionsRepository questionsRepository;
 
     @Override
     public String getCalculateScore(Map<String, String> ansMap) {
 
-
-
-
-        return "";
+        List<Question> listofQue = questionsRepository.findAll();
+            int score= 0;
+        for(Question que: listofQue){
+            if(ansMap.get(que.getId()).equals(que.getAnswer()) ){
+                score++;
+            }
+        }
+        return String.valueOf(score);
     }
 }
