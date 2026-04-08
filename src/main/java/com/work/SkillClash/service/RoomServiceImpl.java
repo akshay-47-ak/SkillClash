6e3f96d64e0028caf3fac51b254b9797dce4e238
akshay-47-ak <akshayathavale1525@gmail.com>
2026-04-08 14:49:47 +0530

    Created the Room Creation Logic

--- src/main/java/com/work/SkillClash/service/RoomServiceImpl.java
+++ src/main/java/com/work/SkillClash/service/RoomServiceImpl.java
@@ -1,11 +1,18 @@
 package com.work.SkillClash.service;
 
 import com.work.SkillClash.dto.RoomRequest;
+import com.work.SkillClash.model.MemberRole;
 import com.work.SkillClash.model.Room;
 import com.work.SkillClash.model.RoomMember;
 import com.work.SkillClash.model.RoomStatus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomServiceImpl implements RoomService {
+
+
+
     @Override
     public String createRooms(RoomRequest roomRequest) {
 
@@ -13,7 +20,18 @@ public class RoomServiceImpl implements RoomService {
                 .status(RoomStatus.LIVE)
                 .build();
 
+        RoomMember rm = roomRequest.getRoomMemberList().get(0);
+              RoomMember roomMember = RoomMember.builder()
+                      .score(rm.getScore())
+                      .role(MemberRole.HOST)
+                      .username(rm.getUsername())
+                      .room(room)
+                      .build();
 
+        room.setHost(roomMember);
+        room.setRoomMemberList(List.of(roomMember));
+
+       Room saveRoom = roomRepository.save(room);
 
         return "";
     }
